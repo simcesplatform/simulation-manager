c6ce4c72b3d12112b8da3bdadd008ef341631430
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-07-07 18:25:36 +0300

    Added class for handling a list of simulation components.

---
+++ tools/components.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains a class for keeping track of the simulation components."""
+
+import tools.tools as tools
+
+FILE_LOGGER = tools.get_logger(__name__)
+
+
+class SimulationComponents():
+    """Keeps a list of components for the simulation and the latest epoch number
+       for which a ready message was received from the component."""
+    NO_MESSAGES = -1
+
+    def __init__(self):
+        self.__components = {}
+        FILE_LOGGER.debug("New SimulationComponents object created.")
+
+        # invariant: self.__latest_full_epoch <= for all self.__components[component_value]
+        self.__latest_full_epoch = SimulationComponents.NO_MESSAGES
+
+    def add_component(self, component_name: str):
+        """Adds a new component to the simulation component list.
+           If the given component_name is already in the list, the function prints an error message."""
+        if component_name not in self.__components:
+            self.__components[component_name] = SimulationComponents.NO_MESSAGES
+            FILE_LOGGER.info("Component: %s registered to SimulationComponents.", component_name)
+        else:
+            FILE_LOGGER.warning("%s is already registered to the simulation component list", component_name)
+
+    def remove_component(self, component_name: str):
+        """Removes the given component from the simulation component list.
+           If the given component_name is not found in the list, the function prints an error message."""
+        if self.__components.pop(component_name, None) is None:
+            FILE_LOGGER.warning("%s was not found in the simulation component list", component_name)
+        else:
+            FILE_LOGGER.info("Component: %s removed from SimulationComponents.", component_name)
+
+        self._update_latest_full_epoch()
+
+    def register_ready_message(self, component_name: str, epoch_number: int):
+        """Registers a new ready message for the given component and epoch number."""
+        if component_name not in self.__components:
+            FILE_LOGGER.warning("%s was not found in the simulation component list", component_name)
+        elif epoch_number < 1:
+            FILE_LOGGER.warning("%d is not acceptable epoch number", epoch_number)
+        elif epoch_number <= self.__components[component_name]:
+            FILE_LOGGER.warning("%d is not larger epoch number than the previous %d",
+                                epoch_number, self.__components[component_name])
+        else:
+            if (epoch_number != self.__components[component_name] + 1 and
+                    self.__components[component_name] != SimulationComponents.NO_MESSAGES):
+                FILE_LOGGER.warning("%d is not the next epoch, previous was %d",
+                                    epoch_number, self.__components[component_name])
+            self.__components[component_name] = epoch_number
+            self._update_latest_full_epoch()
+            FILE_LOGGER.info("Ready message for epoch %d from component %s registered.",
+                             epoch_number, component_name)
+
+    def get_component_list(self, latest_epoch_less_than=None):
+        """Returns a list of the registered simulation components."""
+        if latest_epoch_less_than is None:
+            return list(self.__components.keys())
+        return [
+            component_name
+            for component_name, latest_epoch_for_component in self.__components.items()
+            if latest_epoch_for_component < latest_epoch_less_than
+        ]
+
+    def get_latest_epoch_for_component(self, component_name: str):
+        """Returns the latest epoch number for which the component has responded with a ready message."""
+        return self.__components.get(component_name, None)
+
+    def get_latest_full_epoch(self):
+        """Returns the latest epoch number for which all registered components have responded with a ready message."""
+        return self.__latest_full_epoch
+
+    def __str__(self):
+        """Returns a list of the component names with the latest epoch numbers given in parenthesis after each name."""
+        return ", ".join([
+            "{:s} ({:d})".format(component_name, latest_epoch_for_component)
+            for component_name, latest_epoch_for_component in self.__components.items()
+        ])
+
+    def _update_latest_full_epoch(self):
+        """Updates the value for the latest full epoch."""
+        self.__latest_full_epoch = min(self.__components.values())
---
+++ tools/tests/components.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+"""Unit test for the components module."""
+
+import unittest
+
+from tools.components import SimulationComponents
+
+NO_MESSAGES = -1
+
+
+class TestSimulationComponents(unittest.TestCase):
+    """Unit tests for the SimulationComponents class."""
+
+    def test_new_class(self):
+        """Unit test for an empty component list."""
+        self.assertEqual(SimulationComponents.NO_MESSAGES, NO_MESSAGES)
+
+        components = SimulationComponents()
+        self.assertEqual(components.get_component_list(), [])
+        self.assertEqual(components.get_latest_full_epoch(), NO_MESSAGES)
+        self.assertEqual(str(components), "")
+        self.assertEqual(components.get_latest_epoch_for_component("test"), None)
+
+    def test_add_and_remove_components(self):
+        """Tests adding new components."""
+        new_component_names = ["dummy", "generator", "extra", "planner", "logger"]
+        removed_component_names = ["dummy", "extra", "logger"]
+        remaining_component_names = ["generator", "planner"]
+
+        # add new components
+        components = SimulationComponents()
+        for component_name in new_component_names:
+            components.add_component(component_name)
+
+        self.assertEqual(components.get_component_list(), new_component_names)
+        self.assertEqual(components.get_latest_full_epoch(), NO_MESSAGES)
+        self.assertEqual(str(components), ", ".join([
+            "{:s} ({:d})".format(component_name, NO_MESSAGES)
+            for component_name in new_component_names
+        ]))
+
+        self.assertEqual(components.get_latest_epoch_for_component("test"), None)
+        for component_name in new_component_names:
+            self.assertEqual(components.get_latest_epoch_for_component(component_name), NO_MESSAGES)
+
+        # remove some components
+        for component_name in removed_component_names:
+            components.remove_component(component_name)
+
+        self.assertEqual(components.get_component_list(), remaining_component_names)
+        self.assertEqual(components.get_latest_full_epoch(), NO_MESSAGES)
+        self.assertEqual(str(components), ", ".join([
+            "{:s} ({:d})".format(component_name, NO_MESSAGES)
+            for component_name in remaining_component_names
+        ]))
+
+        for component_name in removed_component_names:
+            self.assertEqual(components.get_latest_epoch_for_component(component_name), None)
+        for component_name in remaining_component_names:
+            self.assertEqual(components.get_latest_epoch_for_component(component_name), NO_MESSAGES)
+
+    def test_simulation_phases(self):
+        """Tests the latest epoch calculations."""
+        new_component_names = ["generator", "planner", "logger"]
+        component_names_expect_first = new_component_names[1:]
+        component_names_expect_last = new_component_names[:len(new_component_names)-1]
+        component_names_expect_middle = new_component_names[:len(new_component_names) // 2] + \
+            new_component_names[len(new_component_names) // 2 + 1:]
+        ready_messages = {
+            1: (component_names_expect_first, NO_MESSAGES),
+            2: (new_component_names, 2),
+            3: (new_component_names, 3),
+            4: (component_names_expect_first, 3),
+            5: (component_names_expect_first, 3),
+            6: (component_names_expect_middle, 5),
+            7: (component_names_expect_last, 6),
+            8: (new_component_names, 8)
+        }
+
+        components = SimulationComponents()
+        for component_name in new_component_names:
+            components.add_component(component_name)
+
+        for epoch_number, (component_names, full_epoch) in ready_messages.items():
+            for epoch_component_name in component_names:
+                components.register_ready_message(epoch_component_name, epoch_number)
+
+            self.assertEqual(components.get_latest_full_epoch(), full_epoch)
+
+            for actual_component_name in new_component_names:
+                self.assertGreaterEqual(components.get_latest_epoch_for_component(actual_component_name), full_epoch)
+                if actual_component_name in component_names:
+                    self.assertEqual(components.get_latest_epoch_for_component(actual_component_name), epoch_number)
+                else:
+                    self.assertLess(components.get_latest_epoch_for_component(actual_component_name), epoch_number)
+
+
+if __name__ == '__main__':
+    unittest.main()
