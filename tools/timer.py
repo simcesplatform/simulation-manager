64e4ea41d41f22ea682d030a27e160cfff06cda2
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-08-12 13:26:49 +0300

    Added the possibility of arguments for timer callbacks.

--- tools/timer.py
+++ tools/timer.py
@@ -9,15 +9,17 @@ import asyncio
 
 class Timer:
     """Timer class that sets up a timed task that can be cancelled."""
-    def __init__(self, timeout, callback):
+    def __init__(self, timeout, callback, *args, **kwargs):
         self.__timeout = timeout
         self.__callback = callback
+        self.__args = args
+        self.__kwargs = kwargs
         self.__task = asyncio.ensure_future(self.__sleep_job())
 
     async def __sleep_job(self):
         """Sleeps and calls the callback function after the sleep."""
         await asyncio.sleep(self.__timeout)
-        await self.__callback()
+        await self.__callback(*self.__args, **self.__kwargs)
 
     def cancel(self):
         """Cancels the sleep job."""
